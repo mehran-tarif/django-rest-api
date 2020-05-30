1b98d83c2891b706c9917b77cb38e48e3d517de4
Mehran Tarif <mrmntf@riseup.net>
2020-05-31 00:26:28 +0430

    model Course created

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Course(models.Model):
+	title	= models.CharField(max_length=160)
+	link	= models.URLField()
+	teacher	= models.ForeignKey(User, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.title
