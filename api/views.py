ae5941e1f7346495473b1634035fb9b132e32fce
Mehran Tarif <mrmntf@riseup.net>
2020-05-31 00:33:38 +0430

    view CourseView created

--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Course
+from .serializers import CourseSerializer
 
 # Create your views here.
+class CourseView(viewsets.ModelViewSet):
+	queryset = Course.objects.all()
+	serializer_class = CourseSerializer
