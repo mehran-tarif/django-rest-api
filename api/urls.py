5326038d730896ab6a6bab9a6a4bca16d6d54fae
Mehran Tarif <mrmntf@riseup.net>
2020-05-31 00:43:22 +0430

    router created

---
+++ api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+router.register('', views.CourseView)
+
+urlpatterns = [
+	path('v1/', include(router.urls)),
+]
