f9cddae34969b94989f82ec1f6ea93ab81be565e
Mehran Tarif <mrmntf@riseup.net>
2020-05-31 01:25:17 +0430

    JWT

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_simplejwt import views as jwt_views
 from . import views
 
 router = routers.DefaultRouter()
@@ -8,4 +9,6 @@ router.register('user', views.UserView)
 
 urlpatterns = [
 	path('v1/', include(router.urls)),
+	path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+	path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- config/settings.py
+++ config/settings.py
@@ -124,5 +124,8 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
 }
